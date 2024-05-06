9f83dab34405d1c06beeeb4f061e83af71e0ee2e
Matthew-Dreemurr <hadadmat@gmail.com>
2024-05-06 15:57:47 +0000

    chore: Update Dockerfile.ptero-egg and entrypoint.sh for bot setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /home/container
 
+git clone https://github.com/fluffy-kaiju/scape-and-run-bot.git bot
+
+npm ci
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
