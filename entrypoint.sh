a79bf6bd432dedb6c9e45e588f7af3b2dbdda131
Matthew-Dreemurr <hadadmat@gmail.com>
2024-05-06 20:18:32 +0000

    Rework to use pterodactyl install logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 cd /home/container
 
+if [ ! -d "/home/container/scape-and-run-bot" ]; then
+	echo "Cloning the bot repository..."
+	git clone https://github.com/fluffy-kaiju/scape-and-run-bot.git
+	echo "Cloning complete!"
+else
+	echo "Pulling the latest changes..."
+	cd /home/container/scape-and-run-bot
+	git pull
+	echo "Pulling complete!"
+fi
+
+cd /home/container/scape-and-run-bot/bot
+
+# Install Dependencies
+echo "Installing dependencies..."
+npm ci
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
