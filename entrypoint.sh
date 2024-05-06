1d5fe07911e192fab9733d5f1d3708603f745722
Matthew-Dreemurr <hadadmat@gmail.com>
2024-05-06 16:46:06 +0000

    chore: Update entrypoint.sh for bot setup and dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 cd /home/container
 
-echo Downloading Bot >> log
-
-git clone https://github.com/fluffy-kaiju/scape-and-run-bot.git bot
-
-cd /home/container/bot
-
-npm ci
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
